56131c48f182e43d5a36311abb67446b84103562
MAC <user@Chides-Technologies.local>
2023-07-15 11:17:32 +0100

    interfacing

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL : "http://localhost:5000/api",
     // baseURL : "http://localhost:5000/api",
+    baseURL : "https://company-mananagement-server.onrender.com/api",
     headers : {
         "Content-Type" : "application/json"
     }
