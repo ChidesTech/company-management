d78b3ac083b76fc7c2ec417419b529faf611eb16
MAC <user@Chides-Technologies.local>
2023-07-15 11:46:30 +0100

    node-engine-addition

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const http = axios.create({
     // baseURL : "http://localhost:5000/api",
-    baseURL : "https://company-mananagement-server.onrender.com/api",
+    baseURL : "https://company-management-server-x7nt.onrender.com/api",
     headers : {
         "Content-Type" : "application/json"
     }
